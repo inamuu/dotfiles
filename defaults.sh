c8beeadd7ce9de987ce31314a66ab224131274d2
inamuu <8310973+inamuu@users.noreply.github.com>
2022-09-05 22:35:40 +0900

    Add a script that set system environment

---
+++ defaults.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### Dock
+defaults write com.apple.dock orientation -string "left"
+defaults write com.apple.dock autohide -bool true
+
+killall Dock
+
+## Mouse & TrackPad
+defaults write -g com.apple.mouse.scaling
+defaults write -g com.apple.trackpad.scaling 5
+
+## Key Repeat
+defaults write -g InitialKeyRepeat -int 12
+defaults write -g KeyRepeat -int 1
+
+echo "Please Reboot"
