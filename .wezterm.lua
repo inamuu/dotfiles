9f1a2998b25f50366cb1d6638a2b42042bafdf30
inamuu <8310973+inamuu@users.noreply.github.com>
2024-06-12 19:46:19 +0900

    modify wezterm splitpane

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,18 +6,39 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 local config = {
-  font = wezterm.font { 
+
+  -- Font
+  font = wezterm.font {
     family = 'Hack Nerd Font Mono', weight = 'Bold',
   },
   font_size = 16.0,
 
--- Color Scheme
--- For example, changing the color scheme:
--- color_scheme = 'Darcula (base16)'
-color_scheme = 'Dracula (Official)'
--- color_scheme = 'darkmoss (base16)'
--- color_scheme = 'duskfox'
+  -- Color Scheme
+  -- color_scheme = 'Dracula (Official)',
+  -- color_scheme = 'Dracula+',
+  color_scheme = 'Dracula (Gogh)',
+  -- color_scheme = 'darkmoss (base16)',
+  -- color_scheme = 'SeaShells',
 
+  -- Window
+  keys = {
+    {
+      key = '|',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.SplitPane {
+        direction = 'Right',
+        size = { Percent = 50 },
+      },
+    },
+  {
+      key = '%',
+      mods = 'CTRL|SHIFT',
+      action = wezterm.action.SplitPane {
+        direction = 'Down',
+        size = { Percent = 50 },
+      },
+    }
+  }
 }
 
 -- and finally, return the configuration to wezterm
