a150e1b501cf03802aa5b80437fb2fd37a35922c
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2025-08-29 00:24:54 +0900

    add test resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+resource "local_file" "test_copy_dotfiles" {
+  content         = file("${path.module}/.terraform-version")
+  filename        = "/tmp/.terraform-version"
+  file_permission = "0644"
+}
+
 resource "terraform_data" "brew_bundle" {
   provisioner "local-exec" {
     command = "brew bundle --file ../Brewfile"
@@ -8,7 +14,7 @@ resource "terraform_data" "brew_bundle" {
 }
 
 resource "local_file" "copy_dotfiles" {
-  for_each        = toset(local.dotfiles)
+  for_each        = toset(local.dotfiles_list)
   content         = file("../${each.value}")
   filename        = pathexpand("~/${each.value}")
   file_permission = "0644"
