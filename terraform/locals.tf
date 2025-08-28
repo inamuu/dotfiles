83d5a28904b09a9203ff3edcb5ecf3cff341c546
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2025-08-28 17:42:54 +0900

    output dotfiles

---
+++ terraform/locals.tf
@@ -0,0 +1,20 @@
+locals {
+  # ファイル一覧を生成
+  all_dotfiles = fileset("${abspath(path.module)}/..", ".*")
+
+  # 除外ファイル一覧
+  exclude_files = [
+    ".DS_Store",
+    ".gitignore"
+  ]
+  # 除外ファイルを除外したファイル一覧
+  dotfiles = [ for f in local.all_dotfiles : f if !contains(local.exclude_files, f)]
+
+  # デプロイ対象のdotfiles
+  dotfiles_json = jsonencode(local.dotfiles)
+}
+
+output "deploy_files_list" {
+  value = local.dotfiles_json
+}
+
