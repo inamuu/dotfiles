b5292d6120a653f542acea6b1b5a67783d26a496
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2025-08-28 17:13:29 +0900

    test local_file

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,5 +2,12 @@ terraform {
   backend "local" {
     path = "terraform.tfstate"
   }
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5.3" 
+    }
+  }
 }
 
