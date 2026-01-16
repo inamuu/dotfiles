29eadff2288267d789c71d83dc8f40cb339d971a
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-01-16 15:51:58 +0900

    colorsheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,14 @@
 
-
-
 vim.opt.clipboard = "unnamedplus"
 
 require("config.lazy")
-)
+
+vim.opt.number = true
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 0 then
+      vim.cmd("NvimTreeOpen")
+    end
+  end,
+})
