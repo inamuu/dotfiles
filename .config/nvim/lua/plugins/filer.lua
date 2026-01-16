5d4927b3ba62ecb190fcf7bcefe7cc597511a85c
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-01-16 15:51:58 +0900

    colorsheme

--- .config/nvim/lua/plugins/filer.lua
+++ .config/nvim/lua/plugins/filer.lua
@@ -2,6 +2,10 @@ return {
   {
     "nvim-tree/nvim-tree.lua",
     opts = {},
+    config = true,
+    keys = {
+	  {mode = "n", "<C-n>", "<cmd>NvimTreeToggle<CR>", desc = "NvimTreeをトグルする"},
+    }
   },
 }
 
