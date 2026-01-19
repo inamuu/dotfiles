ff7262556c57060027dcfb8957ebe92d6fb6f324
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-01-19 14:46:41 +0900

    disable netrw only use neovimtree

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -1,12 +1,18 @@
 return {
   {
     "nvim-tree/nvim-tree.lua",
-    opts = {
-      filters = {
-        dotfiles = false,
-      },
-    },
-    config = true,
+    lazy = false,
+    config = function()
+      require("nvim-tree").setup({
+        filters = {
+          dotfiles = false,
+          git_ignored = false,
+        },
+        view = {
+          width = 30,
+        },
+      })
+    end,
     keys = {
 	  {mode = "n", "<C-n>", "<cmd>NvimTreeToggle<CR>", desc = "NvimTreeをトグルする"},
     }
