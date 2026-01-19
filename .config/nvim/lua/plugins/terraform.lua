fd3ea32645a1402956338446545721ca74609776
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-01-19 14:03:47 +0900

    enable neovim terraform-ls

---
+++ .config/nvim/lua/plugins/terraform.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        terraformls = {},
+        tflint = {},
+      },
+    },
+  },
+  {
+    "hashivim/vim-terraform",
+    ft = { "terraform", "hcl" },
+    config = function()
+      vim.g.terraform_fmt_on_save = 1
+      vim.g.terraform_align = 1
+    end,
+  },
+}
