afecbde55f09109d3569694355a1bb2b20bdd656
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-01-30 12:25:37 +0900

    Enable auto copy from yank values to clipboard

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.lsp.foldexpr()"
 vim.opt.foldlevel = 0
 vim.opt.foldlevelstart = 0
+
+--- yankしたら自動でClipboard連携
+vim.opt.clipboard = "unnamedplus"
