a3315f2ac510ce7acd0305157ed4cf330d5cf788
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-03-18 16:02:10 +0900

    delete comment out lines

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,53 +13,6 @@ resource "terraform_data" "brew_bundle" {
   }
 }
 
-# Copy dotfiles to home directory
-#resource "local_file" "copy_dotfiles" {
-#  for_each        = toset(local.dotfiles_list)
-#  content         = file("../${each.value}")
-#  filename        = pathexpand("~/${each.value}")
-#  file_permission = "0644"
-#}
-
-### 差分取り込み
-#resource "terraform_data" "check_and_copy_dotfiles" {
-#  for_each = var.sync ? toset(local.dotfiles_list) : []
-#
-#  provisioner "local-exec" {
-#    command = <<-EOT
-#      if ! /usr/bin/diff -q "${pathexpand("~/${each.value}")}" "${path.module}/../${each.value}" >/dev/null 2>&1; then
-#          cp -f "${pathexpand("~/${each.value}")}" "${path.module}/../${each.value}"
-#          echo "コピー完了: ${each.value}"
-#      else
-#        echo "差分なし: ${each.value}"
-#      fi
-#    EOT
-#  }
-
-#triggers_replace = {
-#  # ファイルのハッシュが変更された場合に再実行
-#  file_hash = fileexists("../${each.value}") ? filesha256("../${each.value}") : "absent"
-#  # ホームディレクトリのファイルハッシュも含める
-#  home_file_hash = fileexists(pathexpand("~/${each.value}")) ? filesha256(pathexpand("~/${each.value}")) : "absent"
-#}
-#}
-
-#data "external" "sig" {
-#  for_each = local.defaults_apps
-#  program = ["bash", "-lc", <<-EOF
-#VALUE=$(defaults read ${each.value.app} ${each.value.params} 2>/dev/null || true)
-#if [ -z "$VALUE" ]; then
-#  printf "{ \"value\": \"${each.value.value}\" }"
-#else
-#  printf '%s' "$VALUE" | jq -Rs "{ \"${each.value.value}\": .}"
-#fi
-#EOF
-#  ]
-#  query = {
-#    timestamp = timestamp()
-#  }
-#}
-
 resource "terraform_data" "defaults_app" {
   for_each = local.defaults_apps
   provisioner "local-exec" {
