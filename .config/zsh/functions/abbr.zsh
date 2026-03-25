8175a75bf11fe0b7990f4afcee21e3dd9c8cb111
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-03-25 18:43:20 +0900

    abbr add git worktree

--- .config/zsh/functions/abbr.zsh
+++ .config/zsh/functions/abbr.zsh
@@ -47,6 +47,18 @@ abbr -qq -S -f 'tffa=terraform state list | fzf --multi --preview "terraform sta
 
 # .gitconfigがあれば展開はされるけどzsh上定義しておかないと
 # 存在しないコマンドとして赤色になるのでそれをさけるために定義
+gwtadd() {
+  local name=${1:?usage: gwta <name> [base]}
+  local base=${2:-HEAD}
+  git worktree add -b "${name}" "../${name}" "${base}"
+}
+
+gwtremove() {
+  local target=${1:-$(git worktree list --porcelain | awk '/^worktree /{print $2}' | tail -n +2 | fzf)}
+  [[ -n "${target}" ]] || return 0
+  git worktree remove "${target}"
+}
+
 abbr -qq -S -f 'g=git'
 abbr -qq -S -f 'gad=git add .'
 abbr -qq -S -f 'gaa=git add -A'
@@ -59,6 +71,8 @@ abbr -qq -S -f 'ghpv=gh pr view -w'
 abbr -qq -S -f 'ghrv=gh repo view -w'
 abbr -qq -S -f 'gcmm=git commit -m "%ABBR_CURSOR%"'
 abbr -qq -S -f 'gbrd=git branch | fzf | xargs git branch -d '
+abbr -qq -S -f 'gwta=gwtadd'
+abbr -qq -S -f 'gwtr=gwtremove'
 abbr -qq -S -f 'gcmb=git add . && git commit -m "backup at $(date +%Y%m%d%H%M)" && git push'
 abbr -qq -S -f 'tt=echo "%ABBR_CURSOR%"'
 
