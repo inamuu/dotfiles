04e6af06bd52680663ebe76b6f70d6276d4aa4ba
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-05-15 11:28:54 +0900

    backup at 202605151128

--- .config/zsh/functions/aws_ssm.zsh
+++ .config/zsh/functions/aws_ssm.zsh
@@ -12,19 +12,42 @@ function login_check () {
  fi
 }
 
-### Bastion on ECS
+### Bastion on ECS / EC2
+# Usage: set_bastion_id [ecs|ec2] [aws_profile]
 function set_bastion_id () {
-  if login_check; then
-    AWS_PROFILE=${1:-default}
-    ENV_SERVICE=$(aws ecs list-services --cluster ${CLUSTER_NAME} --profile ${AWS_PROFILE} | jq -r '.serviceArns[]' | awk -F'/' '{print $(NF-0)}' | fzf --prompt "踏み台コンテナを選択しようず🍣 ")
-    TASK_ID=$(aws ecs list-tasks --cluster ${CLUSTER_NAME} --service-name ${ENV_SERVICE} --query "taskArns[0]" --output text --region ap-northeast-1 --profile ${AWS_PROFILE} | awk -F '/' '{print $3}')
-    CONTAINER_ID=$(aws ecs describe-tasks --cluster ${CLUSTER_NAME} --task ${TASK_ID} --region ap-northeast-1 --profile ${AWS_PROFILE} | jq -r --arg CONTAINER_NAME bastion '.tasks[0].containers[].runtimeId')
+  BASTION_TYPE=${1:-ecs}
+  AWS_PROFILE=${2:-default}
 
+  if ! login_check; then
+    return 1
   fi
+
+  case "${BASTION_TYPE}" in
+    ecs)
+      ENV_SERVICE=$(aws ecs list-services --cluster ${CLUSTER_NAME} --profile ${AWS_PROFILE} | jq -r '.serviceArns[]' | awk -F'/' '{print $(NF-0)}' | fzf --reverse --prompt "踏み台コンテナを選択しようず🍣 ")
+      TASK_ID=$(aws ecs list-tasks --cluster ${CLUSTER_NAME} --service-name ${ENV_SERVICE} --query "taskArns[0]" --output text --region ap-northeast-1 --profile ${AWS_PROFILE} | awk -F '/' '{print $3}')
+      CONTAINER_ID=$(aws ecs describe-tasks --cluster ${CLUSTER_NAME} --task ${TASK_ID} --region ap-northeast-1 --profile ${AWS_PROFILE} | jq -r --arg CONTAINER_NAME bastion '.tasks[0].containers[].runtimeId')
+      BASTION_TARGET="ecs:${CLUSTER_NAME}_${TASK_ID}_${CONTAINER_ID}"
+      ;;
+    ec2)
+      BASTION_TARGET=$(aws ec2 describe-instances --output=text \
+        --query 'Reservations[].Instances[].{id:InstanceId,ip:PrivateIpAddress,name:Tags[?Key==`Name`].Value|[0]}' \
+        --profile ${AWS_PROFILE} \
+        | fzf --reverse --prompt "踏み台EC2を選択しようず🍣 " | cut -f -1)
+      ;;
+    *)
+      echo "Usage: set_bastion_id [ecs|ec2] [aws_profile]" >&2
+      return 1
+      ;;
+  esac
+
+  [ -n "${BASTION_TARGET}" ]
 }
 
+# Usage: ssm_bastion [ecs|ec2] [aws_profile]
 function ssm_bastion () {
-  set_bastion_id && aws ssm start-session --target ecs:${CLUSTER_NAME}_${TASK_ID}_${CONTAINER_ID}
+  set_bastion_id "${1:-ecs}" "${2:-default}" && \
+    aws ssm start-session --profile ${AWS_PROFILE} --target ${BASTION_TARGET}
 }
 
 ### EC2
@@ -34,7 +57,7 @@ function ssm_ec2 () {
     EC2=$(aws ec2 describe-instances --output=text \
       --query 'Reservations[].Instances[].{id:InstanceId,ip:PrivateIpAddress,name:Tags[?Key==`Name`].Value|[0]}' \
       --profile ${AWS_PROFILE} \
-      | fzf --prompt "Select EC2:" | cut -f -1)
+      | fzf --reverse --prompt "Select EC2:" | cut -f -1)
   fi
 
   if [ -n "${EC2}" ];then
@@ -46,15 +69,17 @@ function ssm_ec2 () {
 }
 
 
+# Usage: ssm_redis [ecs|ec2] [aws_profile]
 function ssm_redis () {
   if login_check; then
-    AWS_PROFILE=${1:-default}
+    AWS_PROFILE=${2:-default}
     REDIS_ENDPOINT=$(aws elasticache describe-replication-groups --output=text --query='ReplicationGroups[].NodeGroups[].[[`PrimaryEndpoint`,PrimaryEndpoint.Address],[`ReaderEndpoint`,ReaderEndpoint.Address]][]' \
-      | fzf--prompt "Select Redis Cluster Endpoint:" | cut -f 2)
-    
+      --profile ${AWS_PROFILE} \
+      | fzf --reverse --prompt "Select Redis Cluster Endpoint:" | cut -f 2)
 
-    if set_bastion_id;then
-      aws ssm start-session --target ecs:${CLUSTER_NAME}_${TASK_ID}_${CONTAINER_ID} \
+
+    if set_bastion_id "${1:-ecs}" "${AWS_PROFILE}";then
+      aws ssm start-session --profile ${AWS_PROFILE} --target ${BASTION_TARGET} \
         --document-name AWS-StartPortForwardingSessionToRemoteHost \
         --parameters "{\"host\":[\"${REDIS_ENDPOINT}\"],\"portNumber\":[\"6379\"], \"localPortNumber\":[\"6379\"]}"
     fi
@@ -62,17 +87,19 @@ function ssm_redis () {
 }
 
 ### Aurora
+# Usage: ssm_aurora [ecs|ec2] [aws_profile]
 function ssm_aurora () {
-  if set_bastion_id;then
-    AWS_PROFILE=${1:-default}
+  AWS_PROFILE=${2:-default}
+  if set_bastion_id "${1:-ecs}" "${AWS_PROFILE}";then
     AURORA_ENDPOINT=$(aws rds describe-db-cluster-endpoints --output=text \
       --query='DBClusterEndpoints[].[EndpointType,Endpoint]' \
-      | fzf --prompt "Select Aurora Endpoint:" | cut -f 2)
+      --profile ${AWS_PROFILE} \
+      | fzf --reverse --prompt "Select Aurora Endpoint:" | cut -f 2)
 
     SESSION_COUNT=$(ps aux | grep "aws ssm start-session" | grep -v grep | wc -l | sed 's/ //g')
     DB_LOCAL_PORT=$(expr 3306 + ${SESSION_COUNT})
     echo "\n---\nConnect to ${AURORA_ENDPOINT}.\nPlease run 'mysql -h 127.0.0.1 -p -P ${DB_LOCAL_PORT} -u'"
-    aws ssm start-session --target ecs:${CLUSTER_NAME}_${TASK_ID}_${CONTAINER_ID} \
+    aws ssm start-session --profile ${AWS_PROFILE} --target ${BASTION_TARGET} \
      --document-name AWS-StartPortForwardingSessionToRemoteHost \
      --parameters "{\"host\":[\"${AURORA_ENDPOINT}\"],\"portNumber\":[\"3306\"], \"localPortNumber\":[\"${DB_LOCAL_PORT}\"]}"
   fi
