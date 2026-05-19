f8c7e17569489f395b838eb1caf580d432e5a16b
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-05-19 16:15:06 +0900

    backup at 202605191615

--- .config/zsh/functions/copypaste.zsh
+++ .config/zsh/functions/copypaste.zsh
@@ -1,6 +1,6 @@
 
 copypaste () {
-  pbpaste | gsed -e '/Refreshing state/d' -e '/Creating.../d' -e '/Reading/d' -e '/Read complete/d' -e '/─────────/d' -e '/.*🎸.*inamuu.*/d' -e 's/^[[:space:]]*//' | pbcopy
+  pbpaste | gsed -e '/Refreshing state/d' -e '/Creating.../d' -e '/Reading/d' -e '/Read complete/d' -e '/─────────/d' -e '/.*🎸.*inamuu.*/d' | pbcopy
 }
 
 zle -N copypaste
