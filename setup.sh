760049ab26797b55eb616c6c05cb0b752b472b3f
Kazuma Inamura <8310973+inamuu@users.noreply.github.com>
2026-08-19 09:39:52 +0900

    setup.sh でのインストール通知処理の修正と zprezto の削除

--- setup.sh
+++ setup.sh
@@ -2,19 +2,14 @@
 
 BREWCMDSTATUS=$(which brew)
 if [ $? -ne 0 ]; then
-  msg "Install homebrew"
+  echo "Install homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 MISECMDSTATUS=$(which mise)
 if [ $? -ne 0 ]; then
-  msg "Install mise"
+  echo "Install mise"
   brew install mise
 fi
-# zpreztoからstarshipにしたのでコメントアウト
-#if [ ! -d "${HOME}/.zprezto" ]; then
-#  msg "Install zprezto"
-#  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-#fi
 
 echo "Finished!!"
